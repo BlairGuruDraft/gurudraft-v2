f345b0bd1d482de1cc17832ea080d3d0eefa819c
BlairGuruDraft <blair@hammerhomesrealestate.com>
2026-06-20 10:39:07 -0700

    Fix Navbar import path in terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Navbar from '../components/Navbar';
+import Navbar from './Navbar';
 
 export default function Terms() {
   return (
